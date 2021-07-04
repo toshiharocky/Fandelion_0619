791c716c846aec9b4cf39f168c7a5984677778a7
EC2 Default User <ec2-user@ip-172-31-46-35.ec2.internal>
2021-07-04 07:49:51 +0000

    gym_introductionからbookingへスロットごとの価格情報を引き継ぎ

--- cms/app/Http/Controllers/BookingController.php
+++ cms/app/Http/Controllers/BookingController.php
@@ -47,13 +47,46 @@ class BookingController extends Controller
     public function store(Request $request)
     {
         //
-        $request->session()->all();
-        dd($request->session()->get('gym_id'));
-        
         $user_name =  Auth::user()->name;
-            return view('booking',[
-                'user_name'=>$user_name,
-                ]);
+        
+        
+        $gym_id = $request->session()->get('gym_id');
+        
+        $gym_infos = Gym::where('id', $gym_id)->get();
+        // dd($gym_infos);
+        
+        $gym_title = $gym_infos[0]->gym_title;
+        
+        // ジムのスケジュールを取得する
+        $gym_schedule = DB::table('gyms')
+                    ->join('gym_schedules', 'gyms.id', '=', 'gym_id')
+                    ->select('gym_id','from_time', 'to_time', 'price', 'status', 'day', 'booking_id')
+                    ->where('gym_id',$gym_id)
+                    ->get();
+        $gym_schedule_count = count($gym_schedule);
+        
+        for($i=0; $i<$gym_schedule_count; $i++){
+            $gym_from_times_str = [$gym_schedule[$i]->from_time][0];
+            $gym_to_times_str = [$gym_schedule[$i]->to_time][0];
+            if([$gym_schedule[$i]->booking_id][0] == ""){
+                $gym_status = "○";
+                $price = [$gym_schedule[$i]->price][0];
+            }else{
+                $gym_status = "×";
+                $price = "-";
+            }
+            
+            $gym_open_times[] = array('date' => date("m/d/Y", strtotime($gym_from_times_str)), 'from_time' => date("H:i", strtotime($gym_from_times_str))
+            , 'to_time' => date("H:i", strtotime($gym_to_times_str)), 'status' => $gym_status,  'price' => $price);
+        }
+        // dd($gym_open_times[3000]);
+        
+        
+        
+        
+        return view('booking',[
+            'user_name'=>$user_name,
+            ]);
         
     }
 
